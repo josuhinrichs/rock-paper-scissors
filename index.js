60199fa41c086da2780c251fbc2e6512702c0134
josuhinrichs <josue2001marinho@gmail.com>
2023-02-06 15:18:26 -0300

    Melhorias na experiência de jogo

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ const roundDisplay = document.querySelector('#round-display');
 const playerScore = document.querySelector('#player-score-text');
 const computerScore = document.querySelector('#computer-score-text');
 const restartButton = document.querySelector('#restart-game');
+const computerDisplay = document.querySelector('.computer-choice-text');
+const playerDisplay = document.querySelector('.player-choice-text');
 
 let randomThree = max => Math.floor(Math.random() * max);
 
 function getComputerChoice() {
   let options = ["rock", "paper", "scissors"];
-  return options[randomThree(3)];
-  //return options[1];
+  const choice = randomThree(3);
+
+  switch (options[choice]){
+    case "paper":
+        computerDisplay.textContent = "Papel";
+        break;
+    case "rock":
+        computerDisplay.textContent = "Pedra";
+        break;
+    default:
+        computerDisplay.textContent = "Tesoura";
+        break;
+  }
+  
+  return options[choice];
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -24,33 +39,27 @@ function playRound(playerSelection, computerSelection) {
       switch (computerSelection){
         case "paper":
           roundText.textContent="Papel embrulha Pedra: Você perdeu!";
-          //console.log("Paper eats Rock: You Lose!");
           return "computer";
         default:
           roundText.textContent="Pedra esmaga Tesoura: Você ganhou!";
-          //console.log("Rock breaks Scissors: You Win!");
           return "player";
       }
     case "paper":
       switch (computerSelection){
         case "rock":
           roundText.textContent="Papel embrulha Pedra: Você ganhou!";
-          //console.log("Paper eats Rock: You Win!");
           return "player";
         default:
           roundText.textContent="Tesoura corta Papel: Você perdeu!";
-          //console.log("Scissors cut Paper: You Lose!");
           return "computer";
       }
     default:
       switch (computerSelection){
         case "rock":
           roundText.textContent="Pedra quebra Tesoura: Você perdeu!";
-          //console.log("Rock breaks Scissors: You Lose!");
           return "computer";
         default:
           roundText.textContent="Tesoura corta Papel: Você ganhou!";
-          //console.log("Scissors cut Paper: You Win!");
           return "player";
       }
     }
@@ -59,6 +68,18 @@ function playRound(playerSelection, computerSelection) {
 
 function processRound(playerSelection){
   let result = playRound(playerSelection, getComputerChoice());
+  switch (playerSelection){
+    case "paper":
+        playerDisplay.textContent = "Papel";
+        break;
+    case "rock":
+        playerDisplay.textContent = "Pedra";
+        break;
+    default:
+        playerDisplay.textContent = "Tesoura";
+        break;
+  }
+
   switch(result) {
     case "player":
         playerPoints += 1;
@@ -69,6 +90,7 @@ function processRound(playerSelection){
     default:
         return;
   }
+
   actualRound += 1;
   roundDisplay.textContent = `Round ${actualRound}`;
   computerScore.textContent = `Computador | ${computerPoints} ponto(s)`;
@@ -93,58 +115,11 @@ function restartGame(){
     roundDisplay.textContent = `Inicie o jogo clicando na opção`;
     computerScore.textContent = `Computador | ${computerPoints} ponto(s)`;
     playerScore.textContent = `Você | ${playerPoints} ponto(s)`;
-    roundText.textContent="Você ainda não começou o jogo!";
+    roundText.textContent = "Você ainda não começou o jogo!";
+    playerDisplay.textContent = "Você ainda não começou o jogo!";
+    computerDisplay.textContent = "Você ainda não começou o jogo!";
     restartButton.style.visibility = 'hidden';
 }
-
-/*function getPlayerSelection(){
-  let playerSelection = "";
-  while (
-    playerSelection != "rock" &&
-    playerSelection != "paper" &&
-    playerSelection != "scissors"){
-    playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-  }
-  return playerSelection;
-}
-
-function playGame(){
-  let roundResult,
-  playerSelection,
-  computerSelection,
-  playerScore = 0,
-  computerScore = 0;
-  
-  for (let i = 0; i < 5; i++){
-    playerSelection = getPlayerSelection();
-    computerSelection = getComputerChoice();
-    roundResult = playRound(playerSelection, computerSelection);
-    console.log(roundResult);
-    switch (roundResult){
-      case "player":
-        playerScore += 1
-        break;
-      case "computer":
-        computerScore += 1
-        break;
-      default: i -= 1
-        break;
-    }
-    
-    console.log(`Scoreboard -> You - ${playerScore} | Computer - ${computerScore}`);
-  }
-
-  if (playerScore > computerScore){
-    console.log("Congratulations, you are the winner!");
-  } else {
-    console.log("You lost :(");
-  }
-
-  console.log(`Final Scoreboard -> You - ${playerScore} | Computer - ${computerScore}`);
-  
-}*/
-
-//playGame();
 
 function removeTransition(e) {
   e.target.classList.remove('clicked');
